d7453ab694744240e608d8ad43072281744d80a0
Rashid Mammadov <rashidmammadov@MacBook-Air-Rashid.local>
2023-01-26 00:57:24 +0400

    base dependecies,express scripts and mongo connect

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const app = express()
 
 
-const PORT = config.get('port') || 3000
+const PORT = config.get('port')
 
 mongoose.set('strictQuery', true)
 process.setMaxListeners(0)
@@ -13,7 +13,7 @@ process.setMaxListeners(0)
 async function start() {
     try{
         await mongoose.connect(config.get('mongoUri'))
-        app.listen(3000, () => console.log("App has been started on port ${PORT}"))
+        app.listen(PORT, () => console.log('App has been started on port ' + PORT))
     } catch (e) {
         console.log('Server is not working', e.message)
         process.exit(1)
